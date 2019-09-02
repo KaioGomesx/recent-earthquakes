2ab0397a8c1b44dfa35d9caf2abd9e8f82776ba5
RIS33 <kaiogomes.nave@gmail.com>
2019-09-02 00:15:54 -0300

    finish the algorithm - v0.0.1

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ const main = (api_url) => {
   axios.get(api_url)
 
     .then(async api_response => {
-      const data = await api_response.data.features
+      const { features } = await api_response.data
 
-      for (iterator of data){
+      for (iterator of features){
         let date_of_occurrence = new Date(iterator.properties['time'])
-        console.log(`${date_of_occurrence}`)
-//         console.log(
-// `[*] place of occurrence: ${iterator.properties['place']}
-// [*] `)
+        
+        console.log(
+`[*] place of occurrence: ${iterator.properties['place']}
+[*] Date of occurrence: ${date_of_occurrence}
+[*] Magnitude of occurrence: ${iterator.properties['mag']}
+[*] Longitude of occurrence: ${iterator.geometry.coordinates[0]}
+[*] Latitude of occurrence: ${iterator.geometry.coordinates[1]}
+[*] Depth of occurrence: ${iterator.geometry.coordinates[2]} Km
+`
+        )
       }
-      
     })
-
     .catch(error => console.log(`[*] Error: ${error}`));
 }
 
